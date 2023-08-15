dcc7696d1971f1ff91ce8e20658d028fe80c3fc2
Dhanush G <chanush02@gmail.com>
2023-08-15 16:04:05 +0530

    morgan log tiny

--- app.ts
+++ app.ts
@@ -27,7 +27,7 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(morgan("combined"));
+app.use(morgan("tiny"));
 
 app.use(router);
 
