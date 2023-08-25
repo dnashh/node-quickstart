3bf8b569dfd8031e9dc0e90be9225d3e83243293
Dhanush G <chanush02@gmail.com>
2023-08-26 00:32:27 +0530

    check for role

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -12,10 +12,13 @@ export const getAllUsers = async (_req: Request, res: Response) => {
 export const createNewUser = bigPromise(async (req: Request, res: Response) => {
   const { name, email, username, role } = req.body;
   const userExist = await User.findOne({
-    where: { [Op.or]: [{ name }, { username }] },
+    where: { [Op.or]: [{ email }, { username }] },
   });
   if (userExist) {
     throw new HttpError(400, "User Already Exists");
+  }
+  if(role && !["user", "admin"].includes(role)){
+    throw new HttpError(400, "Role is Not Allowed");
   }
   const user = await User.create({ name, username, email, role });
   res.status(201).json({ user });
